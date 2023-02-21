d1c7204db1829f6135dc53f712b4344934e3d0d5
Git Patrick <patrick.wan@kinchotech.com>
2023-02-21 22:07:19 +0800

    update

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 });
+// app.post("/submit-message", (req, res) => {
+//   // io.emit("chat message", req.query.message);
+//   io.on("connection", (socket) => {
+//     socket.emit("chat message", req.query.message);
+//   });
+//   res.send("OK");
+// });
+// app.get("/get-messages", (req, res) => {
+//   // io.emit("chat message", req.query.message);
+//   io.on("connection", (socket) => {
+//     socket.on("chat message", (msg) => {
+//       // io.emit("chat message", msg);
+//       res.send(msg);
+//     });
+//   });
+// });
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
